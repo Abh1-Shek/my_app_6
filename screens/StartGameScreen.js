1fafa7ef2a6f59cd8cb2e789c9571bf26aa75c1c
Abh1-Shek <2019uee0141@iitjammu.ac.in>
2022-05-02 08:24:31 +0530

    button ripple effect

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,12 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
     return (
         <View style = {styles.inputContainer}>
-            <TextInput />
+            <TextInput 
+                style = {styles.numberInput} 
+                maxLength = {2}
+                keyboardType = 'number-pad'
+                autoCorrect = {false}
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -20,8 +25,23 @@ const styles = StyleSheet.create({
     inputContainer: {
         padding: 16,
         marginTop: 100,
-        backgroundColor: '#72063c',
+        backgroundColor: '#4e0329',
         marginHorizontal: 24,
         borderRadius: 8,
-    }
+        elevation: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.25,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
