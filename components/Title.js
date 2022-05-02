e83b5d1f638eed6b1c131f352dd2bc2fe0c61e97
Abh1-Shek <2019uee0141@iitjammu.ac.in>
2022-05-02 18:21:05 +0530

    add global color file

--- components/Title.js
+++ components/Title.js
@@ -1,5 +1,5 @@
 import { Text, StyleSheet } from 'react-native'
-
+import Colors from '../constants/colors';
 
 function Title({children}) {
     return <Text style={styles.title}>{children}</Text>
@@ -13,10 +13,10 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: '#ddb52f',
+        color: Colors.accent500,
         textAlign: 'center',
         padding: 12,
         borderWidth: 2,
-        borderColor: '#ddb52f',
+        borderColor: Colors.accent500,
     },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { TextInput, View, StyleSheet, Alert } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton';
 import { useState } from 'react'
+import Colors from '../constants/colors';
 
 
 function StartGameScreen({onPickNumber}) {
@@ -53,7 +54,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 16,
         marginTop: 100,
-        backgroundColor: '#4e0329',
+        backgroundColor: Colors.primary800,
         marginHorizontal: 24,
         borderRadius: 8,
         elevation: 10,
@@ -65,7 +66,7 @@ const styles = StyleSheet.create({
         height: 50,
         width: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
         color: '#ddb52f',
         marginVertical: 8,
