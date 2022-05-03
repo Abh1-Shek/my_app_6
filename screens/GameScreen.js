4d916e9d56cf44abb86420c4ba6a07e07590d02d
Abh1-Shek <2019uee0141@iitjammu.ac.in>
2022-05-03 11:20:55 +0530

    now we can transition from game screen to game over screen as soon as we hit the user input number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet, SafeAreaView, Alert } from 'react-native';
 import Title from '../components/ui/Title';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
 
@@ -18,9 +18,19 @@ let minBoundary = 1;
 let maxBoundary = 100;
 
 
-function GameScreen({ userNumber }) {
+function GameScreen({ userNumber, onGameOver }) {
     const initialGuess = generateRandomBetween(1, 100, userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+
+    useEffect(() => {
+        if (currentGuess == userNumber) {
+            onGameOver();
+        }
+        // console.log("curr=", currentGuess);
+        // console.log('user=', userNumber);
+    }, [currentGuess, userNumber, onGameOver]);
+
 
     function nextGuessHandler(direction) { // 'lower' or 'greater'
 
